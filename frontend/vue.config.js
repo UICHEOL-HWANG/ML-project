e73a88a5fb2f0ecbb20dad91809dca6f556281c0
UICHEOL_HWANG <icuchoel@gmail.com>
2024-02-26 01:52:24 +0900

    pushh

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,9 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
-  transpileDependencies: true,
-  outputDir: 'frontend/docs',
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/ML-project/' // GitHub 저장소 이름으로 변경
-    : '/'
+  publicPath: process.env.NODE_ENV === "production" ? "/ML-project/" : "/",
+  outputDir: "docs",
 })
